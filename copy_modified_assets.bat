d98c0a3b76d7bafe129a8b719653233ece19bcd3
Bryan Haley <bryanmhaley@gmail.com>
2022-06-24 23:40:47 -0700

    Add crosshair to grenades, satchel, snark, and tripmine

---
+++ copy_modified_assets.bat
@@ -0,0 +1,3 @@
+@echo off
+robocopy ".\modified_bsps" "C:\Program Files (x86)\Steam\SteamApps\common\Half-Life\triathlon\maps" /e
+robocopy ".\modified_sprites" "C:\Program Files (x86)\Steam\SteamApps\common\Half-Life\triathlon\sprites" /e
--- copymaps.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-robocopy ".\modified_bsps" "C:\Program Files (x86)\Steam\SteamApps\common\Half-Life\triathlon\maps" /e
