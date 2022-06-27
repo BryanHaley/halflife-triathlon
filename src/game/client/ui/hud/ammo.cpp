0b212db5d4835c6d4d3c2d2201db1a75ec210a1c
Bryan Haley <bryanmhaley@gmail.com>
2022-06-27 16:17:54 -0700

    Set default crosshair scale to 2

--- src/game/client/ui/hud/ammo.cpp
+++ src/game/client/ui/hud/ammo.cpp
@@ -304,7 +304,7 @@ bool CHudAmmo::Init()
 
 	CVAR_CREATE("hud_drawhistory_time", HISTORY_DRAW_TIME, 0);
 	CVAR_CREATE("hud_fastswitch", "0", FCVAR_ARCHIVE); // controls whether or not weapons can be selected in one keypress
-	m_pCvarCrosshairScale = CVAR_CREATE("crosshair_scale", "4", FCVAR_ARCHIVE);
+	m_pCvarCrosshairScale = CVAR_CREATE("crosshair_scale", "2", FCVAR_ARCHIVE);
 
 	m_iFlags |= HUD_ACTIVE; //!!!
 
